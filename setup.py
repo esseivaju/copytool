ebde7ad79897d93e9acaaf9df60b5a86e8ab9cab
Julien Esseiva <esseivaju@gmail.com>
2020-08-25 10:22:49 +0200

    Added progressbar

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     description="Tool to copy file using multiple threads and check file integrity after the copy",
     version="0.1",
     packages=find_packages(),
-    scripts=["bin/bns_copytool"]
+    scripts=["bin/bns_copytool"],
+    install_requires=[
+        "progress>=1.5"
+    ]
 )
